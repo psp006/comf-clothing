a651fa2f645a7697d578ed6dfb054120133154f9
psp006 <pranaypandey730@gmail.com>
2020-11-30 19:49:16 +0530

    added sign-in and sign-up page and functionality using firebase for authorization; used redux for state management and also updated our app to redirect to homepage if user is signed in

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,12 +13,36 @@ const config = {
   measurementId: "G-TNEV3HR0LR"
 };
 firebase.initializeApp(config);
+ 
+export const createUserProfileDocument = async (userAuth, additionalData ) => {
+  if(!userAuth) return;
+  const userRef  = firestore.doc(`users/${userAuth.uid}` );
 
+  const SnapShot = userRef.get( );
+
+  if(!SnapShot.exists) {
+    const {displayName, email} = userAuth;
+    const createdAt = new Date( );
+  
+
+  try{
+    await userRef.set({
+      displayName,
+      email,
+      createdAt,
+      ...additionalData
+    })
+  } catch(error) {
+    console.log('error creating user', error.message);
+   }
+ }
+ return userRef;
+};
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
-export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export const signInWithGoogle = ( ) => auth.signInWithPopup(provider);
 
 export default firebase;
