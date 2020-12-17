2769c0f59ec79ddf056686b04b91b72b41ab6353
psp006 <pranaypandey730@gmail.com>
2020-12-17 19:13:21 +0530

    added reducer and selectors for shop page, added collection-overview component and also added collection component in pages

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -2,7 +2,6 @@ export const addItemTOCart = (cartItems, cartItemToAdd) => {
     const existingCartItem = cartItems.find( 
         cartItem => cartItem.id === cartItemToAdd.id
     );
-
     if(existingCartItem){
         return cartItems.map(cartItem =>
             cartItem.id === cartItemToAdd.id
@@ -11,4 +10,20 @@ export const addItemTOCart = (cartItems, cartItemToAdd) => {
         );
     }
     return [...cartItems, {...cartItemToAdd,quantity:1}]
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find( 
+        cartItem => cartItem.id === cartItemToRemove.id
+    );
+    
+    if(existingCartItem.quantity === 1){
+        return cartItems.filter( cartItem => cartItem.id !== cartItemToRemove.id);
+    }
+
+    return cartItems.map(cartItem =>
+        cartItem.id === cartItemToRemove.id
+          ? { ...cartItem, quantity: cartItem.quantity - 1 }
+          : cartItem
+      );
 };
