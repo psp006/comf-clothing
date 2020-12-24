5448d0489cc6e43a5704a09fe6622e7aee26b515
psp006 <pranaypandey730@gmail.com>
2020-12-24 09:08:49 +0530

    fixed the styling of Add To Cart button

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import  { persistGate } from 'redux-persist/integration/react';
+import  { PersistGate } from 'redux-persist/integration/react';
 
 import  {store,persistor} from './redux/store';
 
@@ -15,9 +15,9 @@ import App from './App';
 ReactDOM.render(
   <Provider store={store}>
  <BrowserRouter>
-     <persistGate persistor={persistor} >
+     <PersistGate persistor={persistor} >
           <App/>
-     </persistGate>
+     </PersistGate>
       
   </BrowserRouter>
   </Provider> ,
